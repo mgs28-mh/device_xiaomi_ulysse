09778e264d77e8a0234005ad093bf303ead7ea53
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-04-04 23:52:54 +0800

    ulysse: biometrics: Import FP HIDL from lineage-18.1
    Meanwhile also enable fingerprint support.
    
    HEAD is at 297cf8975e647eea7a6b75d5ee0ecd5e70880037

--- device.mk
+++ device.mk
@@ -29,6 +29,11 @@ $(call inherit-product, device/xiaomi/ulysse-common/ulysse.mk)
 
 # Permissions
 PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_ulysse
 
 # Input
 PRODUCT_COPY_FILES += \
