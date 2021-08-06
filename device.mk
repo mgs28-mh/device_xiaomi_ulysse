c52a03934e02c2419b2facca6131fd2dcacced72
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-08-06 18:31:24 +0800

    ulysse: biometrics: Enable Fingerprint on ugg only
    Change-Id: I719e38f521995b5739adb91a90dd05c3e0e68c49

--- device.mk
+++ device.mk
@@ -26,6 +26,7 @@ $(call inherit-product, device/xiaomi/ulysse-common/ulysse.mk)
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml \
+    $(LOCAL_PATH)/configs/blank.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/blank.xml
 
 # Fingerprint
 PRODUCT_PACKAGES += \
